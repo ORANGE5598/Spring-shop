b6ef4bcbc8935f917cd06e95573959823adb1699
ORANGE5598 <100891571+ORANGE5598@users.noreply.github.com>
2023-05-08 09:46:31 +0900

    Update CartController.java

--- src/main/java/com/shop/controller/CartController.java
+++ src/main/java/com/shop/controller/CartController.java
@@ -1,7 +1,14 @@
 package com.shop.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shop.config.auth.UserAdapter;
@@ -22,4 +29,12 @@ public class CartController {
 		
 		cartService.saveCart(dto, user);
 	}
+	
+	@DeleteMapping("/delete/{cart_id}")
+	public ResponseEntity<?> deleteById(@PathVariable("cart_id") Long cart_id){
+	    cartService.deleteById(cart_id);
+	    return ResponseEntity.noContent().build();
+	}
+	
+	
 }
